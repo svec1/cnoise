f3ebffd77b378899274082e23d8d97107b183b14
svec1 <svec-kto@proton.me>
2026-05-21 18:23:54 +0300

    added copyrigths

--- src/crypto/dh_mlkem.c
+++ src/crypto/dh_mlkem.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Southern Storm Software, Pty Ltd.
+ * Copyright (C) 2026 svec, Pty Ltd.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
